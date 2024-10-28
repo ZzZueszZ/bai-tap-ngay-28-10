4ca4cf84fe992086fa5e3a3c106dceec97d68900
ZzZueszZ <0905071704b@gmail.com>
2024-10-28 13:29:58 +0700

    new

---
+++ bai-tap-28-10/src/main/java/pakira/tech/bai_tap_28_10/BaiTap2810Application.java
@@ -0,0 +1,13 @@
+package pakira.tech.bai_tap_28_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTap2810Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiTap2810Application.class, args);
+	}
+
+}
---
+++ bai-tap-28-10/src/test/java/pakira/tech/bai_tap_28_10/BaiTap2810ApplicationTests.java
@@ -0,0 +1,13 @@
+package pakira.tech.bai_tap_28_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTap2810ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
